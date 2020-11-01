7b2328890dfb6d614ebea790d69cf7aa5818bed3
Boris Kudryavtsev <boriskk.work@gmail.com>
2020-11-01 12:17:34 -0800

    Update lpo_samples and use filename in dataset instead of sample

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # monophonic-instrument-detection
 
 ## Sound samples
-- [London Philharmonic Orchestra (WAV)](https://l.messenger.com/l.php?u=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1y0dccEm9t-Pp1vsMAMfc6jUizqFInYwK%2Fview%3Fusp%3Dsharing&h=AT2_vMLq_A40sCk6MwrAGNrFjA1FJnyD8o1p3Il8tkDALK21y2vP8QYve8BHNWuvMZlL5GkCcu1mGcglxUiKioIMjjlt1PrneUnDENzmT9h-d1xZDWrwFuTMi7DuQ5pdatw-25z9OCZDhAvH2OBmk1S9sXM)
+Download the sound samples and extract into `data/`
+
+- [London Philharmonic Orchestra (WAV)](https://drive.google.com/file/d/17h_2bl1103uxEBxi8qFHRwOSyF-kuxX6/view?usp=sharing)
--- datasets.py
+++ datasets.py
@@ -2,6 +2,8 @@ import numpy as np
 import librosa
 import os
 
+data_dir = 'data'
+
 def get_features(sample):
     y, srate = sample
     mfcc = librosa.feature.mfcc(y, sr=srate)
@@ -12,36 +14,33 @@ def get_features(sample):
 
 def lpo_dataset(shuffle=True, limit=None, sample_dur=1):
     '''Generates training dataset using monophonic single-note instrument sound samples from 
-    the London Philharmonic Orchestra (WAV).
-    '''
-    data_dir = 'lpo_samples'
-    instruments = [item for item in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, item))]
-    sample_dict = {}
+    the London Philharmonic Orchestra (WAV).'''
+    
+    samples_dir = f'{data_dir}/lpo_samples'
+    instruments = [item for item in os.listdir(samples_dir) if os.path.isdir(os.path.join(samples_dir, item))]
+    fn_dict = {}
 
     for instrument in instruments:
-        filenames = librosa.util.find_files(f'{data_dir}/{instrument}', ext=['wav'])
+        filenames = librosa.util.find_files(f'{samples_dir}/{instrument}', ext=['wav'])
         if shuffle:
             np.random.shuffle(filenames)
             
         if limit and limit < len(filenames):
             filenames = filenames[0:limit]
             
-        samples = []
+        fn_dict[instrument] = ['/'.join(fn.split('/')[-3:]) for fn in filenames]
+            
+    dataset = []
+    for label, filenames in fn_dict.items():
         for fn in filenames:
-            y, srate = librosa.load(fn)
+            y, srate = librosa.load(f'{data_dir}/{fn}')
             # zero pad or slice to fit duration
             if len(y) < srate * sample_dur:
                 y = np.pad(y, (0, srate * sample_dur - len(y)), 'constant')
             else:
                 y = y[0:srate * sample_dur]
-            samples.append((y, srate))
                 
-        sample_dict[instrument] = samples
-            
-    dataset = []
-    for label, samples in sample_dict.items():
-        for sample in samples:
-            dataset.append([sample, get_features(sample), label])
+            dataset.append([fn, get_features((y, srate)), label])
 
     return dataset
 
